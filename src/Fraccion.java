89577548987e42e2841df094a3dd53d0f241a231
Manuel Rojas <manuerjs70@gmail.com>
2021-06-16 13:07:19 -0600

    Cambio de diseño fracciones2

--- src/Fraccion.java
+++ src/Fraccion.java
@@ -38,7 +38,7 @@ public class Fraccion {
         restado.mostrar();
         System.out.println("\n---------MULTIPLICACION DE FRACCIONES---------\n");
         fraccion1.mostrar();
-        System.out.print(" × ");
+        System.out.print(" * ");
         fraccion2.mostrar();
         multiplicado = fraccion1.multiplicar(fraccion2);
         System.out.print(" = ");
