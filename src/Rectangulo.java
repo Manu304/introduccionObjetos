912cc5a49c5fe4c6cf6758ad6cd267abd984b1a1
Manuel Rojas <manuerjs70@gmail.com>
2021-06-15 22:21:06 -0600

    Agregue metodos de calcular area y perimetro de rectangulo

--- src/Rectangulo.java
+++ src/Rectangulo.java
@@ -10,4 +10,37 @@ public class Rectangulo {
     public static void main(String[] args) {
         
     }
+
+    private double ancho, largo;
+
+    public Rectangulo(double ancho, double largo) {
+        this.ancho = ancho;
+        this.largo = largo;
+    }
+
+    public double getArea(){
+        return ancho*largo;
+    }
+
+    public double getPerimetro(){
+        return (2*ancho) + (2*largo);
+    }
+
+    public double getAncho() {
+        return ancho;
+    }
+
+    public void setAncho(double ancho) {
+        this.ancho = ancho;
+    }
+
+    public double getLargo() {
+        return largo;
+    }
+
+    public void setLargo(double largo) {
+        this.largo = largo;
+    }
+    
+
 }
