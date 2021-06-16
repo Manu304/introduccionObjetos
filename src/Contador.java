e020a6e63c11d98e9b5c00bcda3bc160cfbae4f6
Manuel Rojas <manuerjs70@gmail.com>
2021-06-15 21:38:45 -0600

    Agregue arcivo contador

---
+++ src/Contador.java
@@ -0,0 +1,17 @@
+package src;
+/*
+202030799 Manuel Rojas
+Crea una clase Contador con los métodos
+para incrementar y decrementar el
+contador. La clase contendrá un
+constructor por defecto, un constructor
+con parámetros, un constructor copia y
+los métodos getters y setters.
+*/
+
+public class Contador {
+    public static void main(String[] args) {
+        Contador ejercicio1 = new Contador();
+
+    }
+}
