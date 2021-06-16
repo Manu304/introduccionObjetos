782fa1230287f30ab8866115b53a2ea7162a76c2
Manuel Rojas <manuerjs70@gmail.com>
2021-06-15 23:44:09 -0600

    Agregue logica en el main de circulo

--- src/Circulo.java
+++ src/Circulo.java
@@ -4,24 +4,40 @@ Cree una clase circulo. Esta clase debe tener
 métodos como calcular el área y el perimetro.
 */
 
+import java.util.Scanner;
+
 public class Circulo {
     public static void main(String[] args) {
-        Circulo ejercicio3 = new Circulo(2);
-        
+        Scanner scanner = new Scanner(System.in);
+        double radioIngresado;
+        System.out.println("\n-------------CREANDO OBJETO CIRCULO-----------\n");
+        do {
+            System.out.print("Ingrese el radio del circulo: ");
+            radioIngresado = scanner.nextDouble();
+            if (radioIngresado < 0) {
+                System.out.println("No puedes ingresar un radio negativo!\n");
+            }
+        } while (radioIngresado < 0);
+        Circulo ejercicio3 = new Circulo(radioIngresado);
+        System.out.println("Circulo de radio " + ejercicio3.getRadio() + " creado!");
+        System.out.println("\n------------CALCULANDO AREA Y PERIMETRO----------\n");
+        System.out.println("Area del circulo: " + ejercicio3.getArea());
+        System.out.println("Perimetro del circulo: " + ejercicio3.getPerimetro());
+        System.out.println("\n---------------------FIN-------------------------\n");
     }
+
     private double radio;
 
-    
-    public Circulo(float radio) {
+    public Circulo(double radio) {
         this.radio = radio;
     }
 
-    public double getArea(){
-        return Math.PI*radio*radio;
+    public double getArea() {
+        return Math.PI * radio * radio;
     }
 
-    public double getPerimetro(){
-        return Math.PI*radio*2;
+    public double getPerimetro() {
+        return Math.PI * radio * 2;
     }
 
     public double getRadio() {
@@ -31,9 +47,4 @@ public class Circulo {
     public void setRadio(double radio) {
         this.radio = radio;
     }
-    
-
-    
-    
-    
 }
