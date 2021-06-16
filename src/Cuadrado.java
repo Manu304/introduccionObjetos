b98080f849f570749d94fd5afce6dd4a5cfea528
Manuel Rojas <manuerjs70@gmail.com>
2021-06-15 21:39:05 -0600

    Agregue archivo cuadrado ejercicio2

---
+++ src/Cuadrado.java
@@ -0,0 +1,14 @@
+package src;
+/*
+Cree una clase cuadrado. Esta clase cuenta con los
+métodos de calcular área, calcular perimetro. El
+constructor debe de permitir ingresar el tamaño
+de un lado.
+*/
+
+public class Cuadrado {
+    public static void main(String[] args) {
+        
+    }
+    
+}
--- src/Ejercicio1.java
+++
@@ -1,7 +0,0 @@
-package src;
-
-public class Ejercicio1 {
-    public static void main(String[] args) {
-
-    }
-}
