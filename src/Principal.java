69ab299b95f7a536fcbfe7bb95ac9af11d8be910
Manuel Rojas <manuerjs70@gmail.com>
2021-06-15 21:51:06 -0600

    Agregue inciso ejercicio 8

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,15 @@
 package src;
+/*
+Cree una clase llamada Principal. Como se
+muestra en la siguiente slide. En la cual deberá
+hacer el llamado de los ejercicios del 1 al 7.
+a. Debe crear al menos un objeto de cada ejercicio.
+b. Crear una segunda variable del tipo del objeto y
+para mostrar el objeto debe utilizar la primera
+y para modificarlo la segunda
+i. Objetivo demostrar que solo apuntan al
+objeto las variables
+*/
 
 public class Principal {
     public static void main(String[] args) {
