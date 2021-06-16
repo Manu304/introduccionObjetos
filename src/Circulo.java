1ece5e72d04cd5de005345911b0163ab0432c1a8
Manuel Rojas <manuerjs70@gmail.com>
2021-06-15 22:07:03 -0600

    Agregue metodos de perimetro y area de circulo

--- src/Circulo.java
+++ src/Circulo.java
@@ -6,7 +6,34 @@ métodos como calcular el área y el perimetro.
 
 public class Circulo {
     public static void main(String[] args) {
+        Circulo ejercicio3 = new Circulo(2);
         
     }
+    private double radio;
+
+    
+    public Circulo(float radio) {
+        this.radio = radio;
+    }
+
+    public double getArea(){
+        return Math.PI*radio*radio;
+    }
+
+    public double getPerimetro(){
+        return Math.PI*radio*2;
+    }
+
+    public double getRadio() {
+        return radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+    
+
+    
+    
     
 }
