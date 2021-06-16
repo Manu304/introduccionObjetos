dfe51548116230466d106270f585667610e3a4bb
Manuel Rojas <manuerjs70@gmail.com>
2021-06-15 21:41:04 -0600

    Agregue archivo rectangulo.java ejercicio4

---
+++ src/Rectangulo.java
@@ -0,0 +1,13 @@
+package src;
+/*
+Cree una clase rectangulo. Esta clase cuenta con
+los métodos de calcular área, calcular perimetro.
+El constructor debe de permitir ingresar ancho y
+el largo. Debe verificar que no sean iguales.
+*/
+
+public class Rectangulo {
+    public static void main(String[] args) {
+        
+    }
+}
