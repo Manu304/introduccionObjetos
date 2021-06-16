0e3d3f38850d2b03a9fbd30ca19cd16a1e998da6
Manuel Rojas <manuerjs70@gmail.com>
2021-06-15 21:45:36 -0600

    Agregue archivo fraccion.java ejercicio7

---
+++ src/Fraccion.java
@@ -0,0 +1,12 @@
+package src;
+/*
+Crea una clase Fraccion con métodos para sumar,
+restar, multiplicar y dividir fracciones.
+*/
+
+public class Fraccion {
+    public static void main(String[] args) {
+        
+    }
+    
+}
