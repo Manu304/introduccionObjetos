100da764d3f16aa8c3b03d425e863fa99086fb82
Manuel Rojas <manuerjs70@gmail.com>
2021-06-15 22:03:12 -0600

    Agregue metodos de perimetro y area de cuadrado

--- src/Cuadrado.java
+++ src/Cuadrado.java
@@ -1,4 +1,5 @@
 package src;
+
 /*
 Cree una clase cuadrado. Esta clase cuenta con los
 métodos de calcular área, calcular perimetro. El
@@ -7,7 +8,29 @@ de un lado.
 */
 public class Cuadrado {
     public static void main(String[] args) {
-        
+
     }
-    
+
+    private float lado;
+
+    public Cuadrado(float lado) {
+        this.lado = lado;
+    }
+
+    public float perimetro(){
+        return 4*lado;
+    }
+
+    public float area(){
+        return lado*lado;
+    }
+
+    public float getLado() {
+        return lado;
+    }
+
+    public void setLado(float lado) {
+        this.lado = lado;
+    }
+
 }
