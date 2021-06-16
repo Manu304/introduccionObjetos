4b5219cce37082f496a80055e08dd9000d95f416
Manuel Rojas <manuerjs70@gmail.com>
2021-06-15 22:43:37 -0600

    Agregue getter y setter de fraccion

--- src/Fraccion.java
+++ src/Fraccion.java
@@ -8,5 +8,23 @@ public class Fraccion {
     public static void main(String[] args) {
         
     }
+    private int numerador, denominador;
+    public Fraccion(int numerador, int denominador) {
+        this.numerador = numerador;
+        this.denominador = denominador;
+    }
+    public int getNumerador() {
+        return numerador;
+    }
+    public void setNumerador(int numerador) {
+        this.numerador = numerador;
+    }
+    public int getDenominador() {
+        return denominador;
+    }
+    public void setDenominador(int denominador) {
+        this.denominador = denominador;
+    }
+    
     
 }
