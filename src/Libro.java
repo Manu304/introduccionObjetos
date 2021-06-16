2ac12191119c8915e8dec85a55dad13e2fe374aa
Manuel Rojas <manuerjs70@gmail.com>
2021-06-15 21:44:42 -0600

    Agregue archivo libro.java ejercicio

---
+++ src/Libro.java
@@ -0,0 +1,15 @@
+package src;
+/*
+Crea una clase Libro con los métodos préstamo,
+devolución y mostrarInformación. La clase
+contendrá un constructor por defecto, un
+constructor con parámetros y los métodos getters
+y setters.
+*/
+
+public class Libro {
+    public static void main(String[] args) {
+        
+    }
+    
+}
