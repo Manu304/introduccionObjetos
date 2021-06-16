8d12173b4b2523f675d45dc0d4bea7b957eb3f8e
Manuel Rojas <manuerjs70@gmail.com>
2021-06-15 21:43:58 -0600

    Agregue inciso

---
+++ src/Contador2.java
@@ -0,0 +1,18 @@
+package src;
+/*
+202030799 Manuel Rojas
+Crea una clase Contador con los métodos para
+incrementar y decrementar el contador. La clase
+contendrá un constructor por defecto, un
+constructor con parámetros, un constructor.
+Generar un método getContador para devolver el
+monto del contador, y un setContador(contador)
+para establecer.
+*/
+
+public class Contador2 {
+    public static void main(String[] args) {
+        
+
+    }
+}
